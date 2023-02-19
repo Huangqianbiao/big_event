975ef5145e25dae3b651dde026f4e1ea1dbb089c
Huangqianbiao <48092630+Huangqianbiao@users.noreply.github.com>
2023-02-19 14:40:54 +0800

    持久存储token到vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    token: '' // 保存 token 字符串
+    // 1. 保存 token 字符串
+    token: ''
   },
   getters: {
   },
   mutations: {
-    // 保存 token
+    // 2. 更新 token 的 mutation 函数
     updateToken (state, val) {
       state.token = val
     }
@@ -18,5 +20,9 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  // 3. 配置为 vuex 的插件
+  plugins: [createPersistedState()]
 })
+
+// vuex 默认保存在内存中，刷新会初始化所有值（无法做到持久化存储）
