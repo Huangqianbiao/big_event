27d209909c1efbfda13db613d3f16fa5cffeb0b7
Huangqianbiao <48092630+Huangqianbiao@users.noreply.github.com>
2023-02-20 10:32:24 +0800

    在路由守卫里获取用户信息

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,7 @@
 </template>
 
 <script>
-// import { mapActions } from 'vuex'
 export default {
-  // ...mapActions(['getUserInfoActions']),
-  created () {
-    // 调用 vuex 中 action 的方法
-    this.$store.dispatch('getUserInfoActions')
-  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
@@ -30,4 +31,18 @@ const router = new VueRouter({
   routes
 })
 
+// 全局前置路由守卫
+router.beforeEach((to, from, next) => {
+  const token = store.state.token
+  if (token) {
+    store.dispatch('getUserInfoActions')
+  }
+  next()
+})
+
 export default router
+
+// 退出登录或重新登录，只走相关组件代码（异步 dom 切换，不会导致所有代码重新执行，App.vue不重新加载）
+// 解决方法：
+// 1. 可以在登录页面，登录成功后，再发请求拿到用户信息
+// 2. 可以在前置路由守卫中，写（路由跳转的时候，判断+获取）
